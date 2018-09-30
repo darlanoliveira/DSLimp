239043a299857e23a38a7821aa1fd7c1de919b5f
darlanoliveira <darlan.first@gmail.com>
2018-09-30 12:03:53 -0300

    Projeto inicial

---
+++ DSlimp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSlimp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
